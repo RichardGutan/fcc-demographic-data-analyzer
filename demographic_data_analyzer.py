9535a2b258e46737ffa663964c41a3d423d504c9
Richard <gutan.richard@gmail.com>
2020-12-26 11:12:52 +0000

    Initial commit

---
+++ demographic_data_analyzer.py
@@ -0,0 +1,89 @@
+import pandas as pd
+
+
+def calculate_demographic_data(print_data=True):
+    # Read data from file
+    df = pd.read_csv('adult.data.csv', delimiter=',')
+   
+    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    race_count = df['race'].value_counts()
+
+    # What is the average age of men?
+    average_age_men = df[df['sex'] == 'Male']['age'].mean().round(1)
+
+    # What is the percentage of people who have a Bachelor's degree?
+    percentage_bachelors = round(len(df[df['education'] == 'Bachelors'].index) / len(df.index) * 100, 1)
+
+    # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
+    # What percentage of people without advanced education make more than 50K?
+
+    # with and without `Bachelors`, `Masters`, or `Doctorate`
+    has_higher = df['education'].isin([
+        'Bachelors', 
+        'Masters', 
+        'Doctorate'
+    ])
+    higher_education = df[has_higher]
+    lower_education = df[~has_higher]
+    # percentage with salary >50K
+    higher_education_rich = round(len(higher_education[higher_education['salary'] == '>50K'].index) / len(higher_education.index) * 100, 1)
+    lower_education_rich = round(len(lower_education[lower_education['salary'] == '>50K'].index) / len(lower_education.index) * 100, 1)
+
+    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours = df['hours-per-week'].min()
+
+    # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
+
+    rich_percentage = round(len(num_min_workers[num_min_workers['salary'] == '>50K'].index) / len(num_min_workers.index) * 100, 1) 
+
+    # What country has the highest percentage of people that earn >50K?
+    grouped = df.groupby('native-country')
+
+    highest_ratio = 0
+    highest_earning_country = None
+    for name, group in grouped:
+        rich_count = len(group[group['salary'] == '>50K'].index)
+        count = len(group.index)
+        ratio = rich_count / count
+
+        if highest_ratio < ratio:
+            highest_ratio = ratio
+            highest_earning_country = name
+    
+
+
+    highest_earning_country_percentage = round(highest_ratio * 100, 1)
+
+    # Identify the most popular occupation for those who earn >50K in India.
+    rich = df['salary'] == '>50K'
+    indian = df['native-country'] == 'India'
+    top_IN_occupation = df[rich & indian]['occupation'].value_counts().index[0]
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+    if print_data:
+        print("Number of each race:\n", race_count) 
+        print("Average age of men:", average_age_men)
+        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(f"Min work time: {min_work_hours} hours/week")
+        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print("Country with highest percentage of rich:", highest_earning_country)
+        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        print("Top occupations in India:", top_IN_occupation)
+
+    return {
+        'race_count': race_count,
+        'average_age_men': average_age_men,
+        'percentage_bachelors': percentage_bachelors,
+        'higher_education_rich': higher_education_rich,
+        'lower_education_rich': lower_education_rich,
+        'min_work_hours': min_work_hours,
+        'rich_percentage': rich_percentage,
+        'highest_earning_country': highest_earning_country,
+        'highest_earning_country_percentage':
+        highest_earning_country_percentage,
+        'top_IN_occupation': top_IN_occupation
+    }
